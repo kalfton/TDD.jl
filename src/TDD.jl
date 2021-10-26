7e627276ef8a1a1de803587b752cfbe3a687e089
kalfton <36215989+kalfton@users.noreply.github.com>
2021-10-25 22:47:17 -0500

    Question 1 finished

--- src/TDD.jl
+++ src/TDD.jl
@@ -1,5 +1,56 @@
 module TDD
+export neighbors, connectednodes, connectedparts
 
-# Write your package code here.
+function neighbors(graph::Vector, startnode::Integer)
+    nodes = union(graph[startnode], startnode)
+    return sort!(nodes)
+end
+
+function neighbors(graph::Matrix{Bool}, startnode::Integer)
+    nodes = findall(graph[startnode,:])
+    union!(nodes, startnode)
+    return sort!(nodes)
+end
+
+function connectednodes(graph::Union{Vector, Matrix{Bool}}, startnode::Integer)
+    # depth first search?
+    nodestack = [startnode]
+    nodes = Vector{Integer}()
+    while !isempty(nodestack)
+        currentnode = pop!(nodestack)
+        push!(nodes, currentnode)
+        neighbornodes = neighbors(graph, currentnode)
+        union!(nodestack, setdiff(neighbornodes, nodes))
+    end
+    return sort!(nodes)
+end
+
+function connectedparts(graph::Union{Vector, Matrix{Bool}})
+    parts=Vector{Vector{Integer}}()
+    for currentnode in 1:size(graph,1)
+        nodes = connectednodes(graph, currentnode)
+        mergeind=[]
+        for currentpart in 1: size(parts,1)
+            if !isempty(intersect(parts[currentpart], nodes))
+                sort!(union!(parts[currentpart], nodes))
+                push!(mergeind, currentpart)
+            end
+                
+        end
+        if size(mergeind,1)==0
+            # if cannot find overlapping in the existing node, create a new one
+            push!(parts, nodes)
+        elseif size(mergeind,1)>1
+            # if there are multiple parts that the current node connected to, merge them all
+            for ii in 2:size(mergeind,1)
+                union!(parts[mergeind[1]], parts[mergeind[ii]])
+            end
+            sort!(parts[mergeind[1]])
+            deleteat!(parts, mergeind[2:end])
+        end
+    end
+    return parts
+end
+
 
 end
---
+++ test/graph_test.jl
@@ -0,0 +1,47 @@
+using Test
+using TDD
+@testset "graph_test" begin
+    graph1 = [
+          [2, 3],     # list of nodes that can be reached from node 1
+          [1],        # list of nodes that can be reached from node 2
+          [1],        # ", node 3
+          [5],        # ", node 4
+          [4]]        # ", node 5
+
+    graph2 = [
+          [2],     # node 1
+          [],      # node 2
+          [2,4],        # ", node 3
+          [],        # ", node 4
+          [4,6],        # ", node 5
+          []]        # ", node 6
+
+    graph3 = Bool[
+        1 1 1 0 0;
+        1 1 0 0 0;
+        1 0 1 0 0;
+        0 0 0 0 1;
+        0 0 0 1 1;
+      ]
+    nodes = neighbors(graph1, 1)
+    @test nodes == [1,2,3]
+    nodes = neighbors(graph2, 4)
+    @test nodes == [4]
+    nodes = neighbors(graph3, 4)
+    @test nodes == [4,5]
+
+    nodes = connectednodes(graph1, 1)
+    @test nodes == [1,2,3]
+    nodes = connectednodes(graph2, 5)
+    @test nodes == [4,5,6]
+    nodes = connectednodes(graph3, 4)
+    @test nodes == [4,5]
+
+    parts=connectedparts(graph1)
+    @test parts==[[1,2,3], [4,5]]
+    parts=connectedparts(graph2)
+    @test parts==[[1,2,3,4,5,6]]
+    nodes = connectedparts(graph3)
+    @test nodes == [[1,2,3], [4,5]]
+
+end;
