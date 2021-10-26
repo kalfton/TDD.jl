6a8c0aba08a9dcd5275308e20e70335fd2b4e0c2
kalfton <36215989+kalfton@users.noreply.github.com>
2021-10-25 22:46:46 -0500

    question 1 finished

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
-using TDD
 using Test
 
-@testset "TDD.jl" begin
-    # Write your tests here.
-end
+@testset verbose = true "TDD" begin
+    include("graph_test.jl");
+    #include("testfile_q2.jl")
+end;
